7bffec8fa230764651c5161dfcda88504887b3a4
Pierre Padrixe <pierre.padrixe@numergy.com>
2014-09-11 16:31:57 +0200

    Add initial files
    Change-Id: I97fce31ea2a4414b6d001d65b0505c2a90bbfc2a

---
+++ README.md
@@ -0,0 +1,6 @@
+Description
+===========
+
+This cookbook installs the OpenStack Data Processing service **Sahara** as part of an OpenStack reference deployment Chef for OpenStack.
+
+https://wiki.openstack.org/wiki/Sahara
---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+
+require 'chefspec'
+require 'chefspec/berkshelf'
+
+ChefSpec::Coverage.start! { add_filter 'openstack-data-processing' }
+
+require 'chef/application'
+
+LOG_LEVEL = :fatal
+SUSE_OPTS = {
+  platform: 'suse',
+  version: '11.3',
+  log_level: LOG_LEVEL
+}
+REDHAT_OPTS = {
+  platform: 'redhat',
+  version: '6.5',
+  log_level: LOG_LEVEL
+}
+UBUNTU_OPTS = {
+  platform: 'ubuntu',
+  version: '12.04',
+  log_level: LOG_LEVEL
+}
