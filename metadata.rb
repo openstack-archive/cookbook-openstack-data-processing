6862481f4aef15ed3256071ceac40c8221a0f534
JJ Asghar <jj@getchef.com>
2015-08-27 15:45:28 -0500

    Initial Liberty Changes
    Metadata changes
    - Updated maintainer email
    - Updated the version
    
    Change-Id: I1ebdde8f4ecf8f3b6d225592e088107dbd260204

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'openstack-data-processing'
 maintainer 'openstack-chef'
-maintainer_email 'opscode-chef-openstack@googlegroups.com'
+maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache 2.0'
 description 'The OpenStack Data Processing service Sahara.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '11.0.0'
+version '12.0.0'
 
 %w( ubuntu redhat suse ).each do |os|
   supports os
